14a11b426d3b8a46b10c6b986a9f989ba2ee3c8a
lavanya1309 <159130578+lavanya1309@users.noreply.github.com>
2025-05-09 11:56:38 +0530

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,11 @@
+function createRoom() {
+  const roomId = Math.random().toString(36).substring(2, 10);
+  window.location.href = `/room/${roomId}`;
+}
 
+function joinMeeting() {
+  const roomId = prompt('Enter meeting ID:');
+  if (roomId) {
+    window.location.href = `/room/${roomId}`;
+  }
+}
