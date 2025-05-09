3d04f947a35740933ecfc5f33d794bdf976c18ab
lavanya1309 <159130578+lavanya1309@users.noreply.github.com>
2025-05-09 14:38:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -76,8 +76,16 @@ app.get('/room/:roomId', isAuthenticated, (req, res) => {
 
 // Socket.IO logic (if needed)
 io.on('connection', socket => {
-  console.log('a user connected');
+  socket.on('join-room', (roomId, userId) => {
+    socket.join(roomId);
+    socket.to(roomId).emit('user-connected', userId);
+
+    socket.on('disconnect', () => {
+      socket.to(roomId).emit('user-disconnected', userId);
+    });
+  });
 });
+
 
 server.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
