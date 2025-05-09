e6dfdb32da4f8e125c8f03db4bfcbbd3f427c6b2
lavanya1309 <159130578+lavanya1309@users.noreply.github.com>
2025-05-09 11:52:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const { v4: uuidv4 } = require('uuid');
 
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/room/:roomId', (req, res) => {
+  res.render('room', { roomId: req.params.roomId });
+});
+
+http.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
